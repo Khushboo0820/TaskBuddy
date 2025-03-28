e4fc259e51e6c29512164a94e1a7345238662aa4
unknown <khushboo.parate74@gmail.com>
2025-03-28 23:07:16 +0530

    firebase file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyA07cTfQ7xPf-QiZ7CUhEQcFYld6q4PUE4",
   authDomain: "taskbuddy-4bb78.firebaseapp.com",
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
-
+ export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
